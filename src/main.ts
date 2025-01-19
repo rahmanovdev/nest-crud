4bcf749c53de1549b859fe3d3c310f0a9e0c7a58
rahmanovdev <jumadilrahmonov@gmail.com>
2025-01-19 16:35:55 +0600

    finish

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  // Swagger конфигурациясы
   const config = new DocumentBuilder()
     .setTitle('Items API')
     .setDescription('Items API документациясы')
     .setVersion('1.0')
+    .addServer(`http://localhost:/${process.env.PORT ?? 3000}`) // Локалдык сервер
+    .addServer(`${process.env.PORT ?? 3000}`) // Продакшн сервер (deploy кылгандан кийин)
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
